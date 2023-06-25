2cbf0f1d184bf252583b53cf645ba48da7093b96
Andrew Anos <96986357+AndrewAnos30@users.noreply.github.com>
2023-06-25 22:00:25 +0800

    obj3 part 3

--- pos/forms.py
+++ pos/forms.py
@@ -70,8 +70,11 @@ class MenuCategoryForm(forms.ModelForm):
 class MenuDrinksForm(forms.ModelForm):
     class Meta:
         model = MenuDrinks
-        fields = ["hotAndCold", "menucategory", "menuname", "menuimage", "menuAOPrice1", "menuAOPrice2", "menuAOPrice3", "menuAOPrice4", "menuAOPrice5","ingredient1", "ingredient2", "ingredient3", "ingredient4", "ingredient5","ingredient6", "ingredient7", "ingredient8", "ingredient9", "ingredient10", "addons1", "addons2", "addons3", "addons4","addons5", "quantityIng1", "quantityIng2", "quantityIng3", "quantityIng4", "quantityIng5", "quantityIng6", "quantityIng7", "quantityIng8", "quantityIng9", "quantityIng10", "quantityAO1", "quantityAO2", "quantityAO3", "quantityAO4", "quantityAO5","menuprice1", "menuprice2", "menuprice3", "question1", "question2", "question3", "question4"]
+        fields = ["hotAndCold", "menucategory", "menuname", "menuimage", "menuAOPrice1", "menuAOPrice2", "menuAOPrice3", "menuAOPrice4", "menuAOPrice5","ingredient1", "ingredient2", "ingredient3", "ingredient4", "ingredient5","ingredient6", "ingredient7", "ingredient8", "ingredient9", "ingredient10", "addons1", "addons2", "addons3", "addons4","addons5", "quantityIng1", "quantityIng2", "quantityIng3", "quantityIng4", "quantityIng5", "quantityIng6", "quantityIng7", "quantityIng8", "quantityIng9", "quantityIng10", "quantityAO1", "quantityAO2", "quantityAO3", "quantityAO4", "quantityAO5","menuprice1", "menuprice2", "menuprice3", "question1", "question2", "question3", "question4", "q1", "q2", "q3"]
         widgets = {
+            'q1' : forms.RadioSelect (choices=((True, 'Yes'), (False, 'No'))),
+            'q2' : forms.RadioSelect (choices=((True, 'Yes'), (False, 'No'))),
+            'q3' : forms.RadioSelect (choices=((True, 'Yes'), (False, 'No'))),
             'question1' : forms.RadioSelect (choices=((True, 'Yes'), (False, 'No'))),
             'question2' : forms.RadioSelect (choices=((True, 'Yes'), (False, 'No'))),
             'question3' : forms.RadioSelect (choices=((True, 'Yes'), (False, 'No'))),
